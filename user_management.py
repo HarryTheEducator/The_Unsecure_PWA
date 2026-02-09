224335c271281dbadd3be1f46e7f5b9e85c087a6
HarryTheEducator <alharith.alkhalaileh@education.nsw.gov.au>
2026-02-09 21:14:34 +0000

    Fix: Implemented output escaping in feedback list to prevent Stored XSS

--- user_management.py
+++ user_management.py
@@ -1,6 +1,7 @@
 import sqlite3 as sql
 import time
 import random
+from markupsafe import escape
 
 
 def insertUser(username, password, DoB):
@@ -57,6 +58,6 @@ def listFeedback():
     f = open("templates/partials/success_feedback.html", "w")
     for row in data:
         f.write("<p>\n")
-        f.write(f"{row[1]}\n")
+        f.write(f"{escape(row[1])}\n")  # Use escape() to neutralise scripts
         f.write("</p>\n")
     f.close()
